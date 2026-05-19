904cb66417f7be7d3dbaab7016c5e869da5acb0e
jerrigogo <jg.sa@netive.co.kr>
2026-05-19 09:10:16 +0900

    tooltip 컴포넌트 수정 배포

---
+++ assets/GuideTooltip-C6rIyXvt.js
@@ -0,0 +1,13 @@
+import{_ as l}from"./AccordionComp-Z8k75R08.js";import{_ as p}from"./TooltipComp-BMdO3SuJ.js";import{c as i,a as s,b as a,j as n,F as e,o as c,k as o}from"./index-9Ffzbz8V.js";const r={class:"guide-cont"},m={class:"guide-cont"},d={class:"guide-cont"},u={class:"guide-cont"},T={__name:"GuideTooltip",setup(v){return(f,t)=>(c(),i(e,null,[t[4]||(t[4]=s("h3",{class:"guide-stitle"},"Default: 'Enter' 키 or 마우스 click",-1)),s("div",r,[a(p,{"tip-title":"개인정보 처리목적"},{default:n(()=>[...t[0]||(t[0]=[s("p",null,"개인정보 처리목적 : 보험계약 상담, 보험계약 인수여부 결정을 위한 판단 등",-1)])]),_:1})]),s("div",m,[a(l,{id:"TooltipClick","acco-title":"Code 보기"},{accoPanel:n(()=>[...t[1]||(t[1]=[s("div",{class:"guide-info script"},[s("pre",null,[o(""),s("span",{class:"comment"},"// component import"),o(`
+`),s("span",{class:"import"},"import"),o(),s("span",{class:"props"},"TooltipComp"),o(),s("span",{class:"import"},"from"),o(),s("span",{class:"val"},"'@/components/TooltipComp.vue'"),o(`;
+`)])],-1),s("div",{class:"guide-info html"},[s("pre",null,[o(""),s("span",{class:"comment"},"<!-- tip-title: 스크린리더 용 title -->"),o(`
+`),s("span",{class:"arrow"},"<"),s("span",{class:"component"},"tooltip-comp"),o(),s("span",{class:"props"},"tip-title"),o('="'),s("span",{class:"val"},"개인정보 처리목적"),o('"'),s("span",{class:"arrow"},">"),o(`
+  `),s("span",{class:"comment"},"// Tooltip contents"),o(`
+  ...
+`),s("span",{class:"arrow"},"</"),s("span",{class:"component"},"tooltip-comp"),s("span",{class:"arrow"},">"),o(`
+`)])],-1)])]),_:1})]),t[5]||(t[5]=s("h3",{class:"guide-stitle"},"마우스 over or 포커스",-1)),s("div",d,[a(p,{"tip-title":"개인정보 처리목적","tip-hover":""},{default:n(()=>[...t[2]||(t[2]=[s("p",null,"개인정보 처리목적 : 보험계약 상담, 보험계약 인수여부 결정을 위한 판단 등",-1)])]),_:1})]),s("div",u,[a(l,{"acco-title":"Code 보기"},{accoPanel:n(()=>[...t[3]||(t[3]=[s("div",{class:"guide-info html"},[s("pre",null,[o(""),s("span",{class:"comment"},"// 'tip-hover' 속성 추가"),o(`
+`),s("span",{class:"arrow"},"<"),s("span",{class:"component"},"tooltip-comp"),o(),s("span",{class:"props"},"tip-title"),o('="'),s("span",{class:"val"},"개인정보 처리목적"),o('" '),s("span",{class:"props"},"tip-hover"),s("span",{class:"arrow"},">"),o(`
+  `),s("span",{class:"comment"},"// Tooltip contents"),o(`
+  ...
+`),s("span",{class:"arrow"},"</"),s("span",{class:"component"},"tooltip-comp"),s("span",{class:"arrow"},">"),o(`
+`)])],-1)])]),_:1})])],64))}};export{T as default};
